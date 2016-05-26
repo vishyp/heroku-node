8f7ddfa228b1e503296d46b9f3e1cfc7fafa3771
Vishy Poosala <vishypoosala@fb.com>
2016-05-25 21:26:12 -0700

    added message handler

--- server.js
+++ server.js
@@ -41,8 +41,17 @@ console.log(req.body);
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
+  if (event.postback) {
+    text = JSON.stringify(event.postback);
+    sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+    continue;
+  }
     if (event.message && event.message.text) {
       text = event.message.text;
+	if (text === 'Generic') {
+	    sendGenericMessage(sender);
+	    continue;
+  	}
         sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
   }
@@ -74,4 +83,53 @@ function sendTextMessage(sender, text) {
 }
 
 
+
+function sendGenericMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
